ff1921a8366c42759cf293eb210f86aa8d54df10
wlong1 <109259315+wlong1@users.noreply.github.com>
2024-04-04 19:53:49 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def secret_santa(people):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    secret_santa()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
