59e00d4a073f1acb95d487b6525f4e91c51e85b3
wlong1 <109259315+wlong1@users.noreply.github.com>
2024-04-04 20:19:31 -0400

    Outline

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
 
 
-def secret_santa(people):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def secret_santa(people, likes, dislikes):
+    # People is a simple list of strings
+    # Likes is a dict with the strings from People as keys and a list of strings as items
+    # Dislikes is formatted the same way as likes
+
+    # Sort participants by highest number of dislikes first to get them "out of the way".
+    # Duplicate People and shuffle list.
+    # Go through the shuffled list looking for valid likes. Swap with last place and popright after.
+
+    # Outputs a list of strings, each going "Gifter -> Recepient"
+
+    deck = []
+    temp = []  # Need a temporary list to hold tuples of (# of Dislikes, Person) pairs.
+    for name in dislikes:
+        count = len(dislikes[name])
+        temp.append((count, name))
+    temp.sort()
+    for x in temp:
+        deck.append(x[1])  # Fill deck from max dislikes to least
+
+    participants = random.sample(people, len(people))
+    temp = []  # Reset temp. Use it to store leftover and skipped.
+    chain = []
+    count = 0  # Count number of successful matches. If 0 after a loop, then it's a dead end
 
 
-# Press the green button in the gutter to run the script.
+
+
+
+
+    return chain, temp  # Return the secret santa chain and a list of unmatched participants
+
+
+
 if __name__ == '__main__':
+    # Dislikes are unweighted. Use None to increase dislike weight for a person.
     secret_santa()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
