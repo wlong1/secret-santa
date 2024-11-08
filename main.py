c7e8ba11b55e77a91cc2b134577a678bcaf32021
wlong <109259315+wlong1@users.noreply.github.com>
2024-11-07 23:35:58 -0500

    Added pair-checking and remainder

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+from collections import defaultdict
 
 
 def secret_santa(people, likes, dislikes, pairs=False):
@@ -72,10 +73,87 @@ def secret_santa(people, likes, dislikes, pairs=False):
     return chain, temp  # Return the secret santa chain and a list of unmatched participants
 
 
+def pair_valid(likes, dislikes, gifter, recipient):
+    for dislike in dislikes[gifter]:
+        if dislike is None:
+            continue
+
+        if dislike in likes[recipient]:
+            return False
+
+    return True
+
+
+def single_cycle(people, likes, dislikes):
+    # Sort participants by highest number of dislikes first to get them "out of the way".
+    # Duplicate People and shuffle list.
+    # Go through the shuffled list looking for valid likes. Swap with last place and popright after.
+
+    # Outputs a list of names where the person in an index is gifting the next index
+    # Also outputs a list of unmatched names
+
+    deck = []
+    biggest = cur = -1
+    index = -1
+    for i, name in enumerate(people):
+        count = len(dislikes[name])
+        if count > biggest:
+            cur = name
+            biggest = count
+            index = i
+
+    for x in people:
+        if index != 0:
+            deck.append(x)
+        index -= 1
+    chain = [cur]
+    leftover = []
+
+    while deck:
+        gifter = chain[-1]  # Most recent entry is current gifter
+        random.shuffle(deck)
+        searching = True
+        found = -1
+        for i, recipient in enumerate(deck):
+            if pair_valid(likes, dislikes, gifter, recipient):
+                found = i
+                break
+        if found == -1:
+            leftover.append(chain.pop(-1))
+            if len(chain) == 0:
+                chain = [deck.pop(0)]
+        else:
+            chain.append(deck.pop(found))
+
+    # Scan for leftovers ONCE
+    length = len(chain)-2
+    failed = []
+    found = False
+    for person in leftover:
+        print("Finding leftovers for " + person)
+        for i in range(length):
+            if pair_valid(likes, dislikes, chain[i], person) and pair_valid(likes, dislikes, person, chain[i+1]):
+                chain.insert(i+1, person)
+                found = True
+                break
+        if found:
+            found = False
+        else:
+            failed.append(person)
+
+    return chain, failed  # Return the secret santa chain and a list of unmatched participants
+
+
 if __name__ == '__main__':
     # Dislikes are unweighted. Use None to increase dislike weight for a person.
-    names = []
-    wants = {}
-    hates = {}
-    res = secret_santa(names, wants, hates)
+    names = ['alice', 'john', 'bob']
+    wants = {'alice': ['landscape'],
+             'john': [],
+             'bob': []
+             }
+    hates = {'alice':[],
+             'john': ['landscape'],
+             'bob': []
+             }
+    res = single_cycle(names, wants, hates)
     print(res)
